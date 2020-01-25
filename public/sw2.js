40190c47e4a52c47fdd861b2e2b7f97d2567236d
Robert Fox <robert@foxs.com>
2020-01-25 10:33:40 -0500

    switching sw file

--- public/sw2.js
+++ public/sw2.js
@@ -21,22 +21,24 @@ self.addEventListener('activate', e => {
 })
 
 self.addEventListener('fetch', e => {
-  console.log('Service Worker Fetching')
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Make clone of response
-        const resClone = res.clone()
-        // Open Cache
-        caches.open(cacheName).then(cache => {
-          // Add response to cache
-          cache.put(e.request, resClone)
+  if (e.request.url.indexOf('http') === 0) {
+    console.log('Service Worker Fetching')
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          // Make clone of response
+          const resClone = res.clone()
+          // Open Cache
+          caches.open(cacheName).then(cache => {
+            // Add response to cache
+            cache.put(e.request, resClone)
+          })
+          return res
         })
-        return res
-      })
-      .catch(err => {
-        caches.match(e.request).then(res => res)
-        console.error(err)
-      })
-  )
+        .catch(err => {
+          caches.match(e.request).then(res => res)
+          console.error(err)
+        })
+    )
+  }
 })
